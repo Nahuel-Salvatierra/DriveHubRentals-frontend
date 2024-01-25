cc240074ac33880451c303399b4bb9ac0c226fa1
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-25 13:54:36 -0300

    Add UI components

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+interface ButtonProps {
+	type: "submit" | "reset" | "button" | undefined;
+	name: string;
+	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+export default function Button({ type, name, onClick }: ButtonProps) {
+	return (
+		<>
+			<button
+				data-cy={name}
+				name={name}
+				type={type}
+				onClick={onClick}
+				className="w-full h-12 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:bg-lime-300"
+			>
+				{name}
+			</button>
+		</>
+	);
+}
