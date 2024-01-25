6d59c25dcbe3088095b7767c7a292dd821e6065a
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-25 13:58:38 -0300

    Add customer main components

---
+++ src/components/customer/CustomerForm.tsx
@@ -0,0 +1,36 @@
+import toast from "react-hot-toast";
+import useCustomer from "../../hooks/useCustomer";
+import { INPUTS_CUSTOMER } from "../../utils/inputs.customer";
+import Button from "../ui/Button";
+import Input from "../ui/Input";
+
+export default function CustomerForm() {
+  
+  const { createCustomer } = useCustomer();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    try {
+          await createCustomer(form);
+    } catch (error) {
+      toast.error(`${error}`);
+    }
+  }
+  return (
+    <div className="flex-auto px-20 pt-10 justify-center">
+      <form onSubmit={handleSubmit}>
+        {INPUTS_CUSTOMER.map((input) => (
+          <Input
+            key={input.name}
+            type={input.type}
+            name={input.name}
+            required={input.required}
+            placeholder={input.placeholder}
+          />
+        ))}
+      <Button type="submit" name="Save" onClick={() => {}}></Button>
+      </form>
+    </div>
+  );
+}
