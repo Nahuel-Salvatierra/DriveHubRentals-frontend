ef73fd6372b170dfea8c89357e453ead11cda254
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-28 17:14:07 -0300

    Add ButtonSubmit component

---
+++ src/components/ButtonSubmit.tsx
@@ -0,0 +1,11 @@
+export default function ButtonSubmit() {
+	return (
+			<button
+				data-cy="button-submit"
+				type="submit"
+				className="text-gray-900 bg-green-300 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+			>
+				Submit
+			</button>
+	);
+}
