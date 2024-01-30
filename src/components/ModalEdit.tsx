935f0163f3d46b66cd8b2d1f1f8bbeb280d2c48b
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-30 10:33:41 -0300

    Add modal form

--- src/components/ModalEdit.tsx
+++ src/components/ModalEdit.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import Customer from "./customer/Customer";
 import { putResources } from "../services/api.service";
 import useSendResources from "../hooks/useSendResources";
+import useBackOffice from "../hooks/useBackOffice";
+import toast from "react-hot-toast";
+import Loading from "./Loading";
+import CustomerInputs from "./customer/CustomerInputs";
 
-export default function ModalEdit({ object, setShowModal }) {
-	const [data, setData] = useState({ ...object });
+export default function ModalEdit({ entity, setShowModal }) {
+	const [data, setData] = useState({ ...entity });
+	const { error, loading, sendResources } = useSendResources();
+	const { selectedOption } = useBackOffice();
 
-	const { sendResources } = useSendResources();
+	const renderForm = () => {
+		switch (selectedOption) {
+			case "customer":
+				return <CustomerInputs data={data} onChange={onChange} />;
+			default:
+				return null;
+		}
+	}
 
 	function onChange(event: React.ChangeEvent<HTMLInputElement>) {
 		setData({
@@ -16,24 +28,18 @@ export default function ModalEdit({ object, setShowModal }) {
 	}
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		try {
-			console.log(`${Customer.name.toLowerCase()}/${object.id}`)
-			await sendResources(
-				putResources,
-				`${Customer.name.toLowerCase()}/${object.id}`,
-				data
-			);
-			setShowModal(false);
-		} catch (error) {
-			console.log(error);
-		}
+		await sendResources(putResources, `${selectedOption}/${data.id}`, data);
+		error
+			? toast.error(`${error.message}`)
+			: toast.success(`${selectedOption.toLowerCase()} updated`);
+		setShowModal(false);
 	};
 	return (
 		<>
 			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-				<div className="relative w-auto my-6 mx-auto max-w-3xl">
+				<div className="relative w-[90%] my-6 mx-auto  max-w-3xl">
 					{/*content*/}
-					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+					<div className="border-0 rounded-lg p-5 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 						{/*header*/}
 						<div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
 							<h3 className="text-3xl font-semibold">Edit</h3>
@@ -47,23 +53,7 @@ export default function ModalEdit({ object, setShowModal }) {
 							</button>
 						</div>
 						<form onSubmit={handleSubmit}>
-							{/*body*/}
-							{Object.entries(object).map(([key, value]) => (
-								<div className="relative p-4 flex" key={key}>
-									<label className=" text-gray-700 text-sm font-bold mb-2">
-										{key}
-									</label>
-									<input
-										name={key}
-										className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-										id="username"
-										type="text"
-										defaultValue={value}
-										onChange={onChange}
-									/>
-								</div>
-							))}
-							{/*footer*/}
+							{renderForm()}
 							<div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
 								<button
 									className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -73,16 +63,18 @@ export default function ModalEdit({ object, setShowModal }) {
 									Close
 								</button>
 								<button
+									data-cy={`submit-modal-${data.id}`}
 									className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 									type="submit"
 								>
-									Save Changes
+									{loading ? <Loading /> : "Save"}
 								</button>
 							</div>
 						</form>
 					</div>
 				</div>
 			</div>
+
 			<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
 		</>
 	);
